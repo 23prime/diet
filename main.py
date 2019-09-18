17d2f6351c1a2ca6977a79ccd3561e528f9e673c
23prime <23.prime.37@gmail.com>
2019-09-18 21:20:51 +0900

    Move processes related to weight to 'Weight' class

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import argparse
-import csv
-import datetime
 import sys
 
 import src.plot as sp
@@ -12,43 +9,22 @@ file_name = 'weight'
 csv_name = file_name + '.csv'
 img_name = file_name + '.png'
 
-# Get command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("weight", help="Please set argument of your today's weight.", type=float)
-args = parser.parse_args()
-t_weight = args.weight
-
-# Get date
-today = str(datetime.date.today().strftime("%m-%d"))
-
-# Get date of first day and yesterday from CSV 
-with open(csv_name) as file:
-    csv_file = csv.reader(file)
-    csv_arr = [row for row in csv_file]
-    if csv_arr[-1][0] == today:
-        print("Error: Today's data is already exist.")
-        exit(1)
-    y_weight = csv_arr[-1][1]
-    f_weight = csv_arr[1][1]
-
-# Get difference of yesterday and first day, and format
-msg = sw.Weight(t_weight, y_weight, f_weight, today).format_weight()
+weight = sw.Weight(csv_name)
+weight_msg = weight.format_weight()
 
 # Ask to do or not
 line = "--------------------\n"
-print("{}{}\n{}Add today's data and post this message with \"{}\"? [Y/n]".format(line, msg, line, img_name))
+print(
+    "{}{}\n{}Add today's data and post this message with \"{}\"? [Y/n]"
+    .format(line, weight_msg, line, img_name))
 ans = input().strip().lower()
+
 if not ans in ['', 'y','yes']:
     print("Canceled.")
     exit(0)
 
-# Update CSV
-with open(csv_name, 'a+') as file:
-    csv.writer(file).writerow([today, t_weight])
-
-# Generate graph from CSV
+# Run
+weight.update_csv()
 sp.Graph().plot_graph()
-
-# Post to Twitter
-st.Tweet().tweet(msg, img_name)
+st.Tweet().tweet(weight_msg, img_name)
 print("Success!")
--- src/weight.py
+++ src/weight.py
@@ -1,3 +1,7 @@
+import argparse
+import csv
+import datetime
+
 def add_sign(f):
     if f < 0:
         return str(f)
@@ -5,17 +9,52 @@ def add_sign(f):
         return "+" + str(f)
 
 class Weight:
-    def __init__(self, t, y, f, today):
-        self.today = today
-        self.t_weight = float(t)
-        y_weight = float(y)
-        f_weight = float(f)
-        self.diff_y = round(self.t_weight - y_weight, 1)
-        self.diff_f = round(self.t_weight - f_weight, 1)
+    def __init__(self, csv_name):
+        self.csv_name = csv_name
+        self.today = str(datetime.date.today().strftime("%m-%d"))
+        self.t_weight = float()
+        self.y_weight = float()
+        self.f_weight = float()
+        self.diff_y = float()
+        self.diff_f = float()
+
+    def today_weight(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "weight", help="Please set argument of your today's weight.",
+            type=float)
+        args = parser.parse_args()
+        self.t_weight = float(args.weight)
+
+    def weights(self):
+        with open(self.csv_name) as file:
+            csv_file = csv.reader(file)
+            csv_arr = [row for row in csv_file]
+
+            if csv_arr[-1][0] == self.today:
+                print("Error: Today's data is already exist.")
+                exit(1)
+
+            self.y_weight = float(csv_arr[-1][1])
+            self.f_weight = float(csv_arr[1][1])
+
+    def diffs(self):
+        self.diff_y = round(self.t_weight - self.y_weight, 1)
+        self.diff_f = round(self.t_weight - self.f_weight, 1)
 
     def format_weight(self):
-        self.t_weight = str(self.t_weight)
+        self.today_weight()
+        self.weights()
+        self.diffs()
+
         self.diff_y = add_sign(self.diff_y)
         self.diff_f = add_sign(self.diff_f)
 
-        return "{}\n{} kg\n前日比： {} kg\n初日比： {} kg\n\n#ok_diet".format(self.today, self.t_weight, self.diff_y, self.diff_f)
+        return ("{}\n{} kg\n前日比： {} kg\n初日比： {} kg\n\n#ok_diet"
+            .format(self.today, self.t_weight, self.diff_y, self.diff_f))
+
+    def update_csv(self):
+        with open(self.csv_name, 'a+') as file:
+            csv.writer(file).writerow([self.today, self.t_weight])
+
+
