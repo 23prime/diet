d703a0a495af98107abb3509e12f4b978a467243
23prime <23.prime.37@gmail.com>
2019-09-22 01:34:38 +0900

    Remove unused import

--- src/plot.py
+++ src/plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib as m
 import matplotlib.pyplot as mp
-from src.weights import Weights, db
+from src.weights import Weights
 
 class Graph:
     def __init__(self):
