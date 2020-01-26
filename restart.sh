4f1bb9d5bcd15b42df6c0691d99a4bd843b37681
23prime <23.prime.37@gmail.com>
2020-01-26 22:06:12 +0900

    Output container logs to AWS/CloudWatchLogs
    If at localhost, use 'docker-compose.local.yml'

--- restart.sh
+++ restart.sh
@@ -1,12 +1,20 @@
+option=''
+
 if [ $# -eq 0 ]; then
     echo '##### git pull #####'
     git pull
-elif [ $1 != '--local' ]; then
-    exit 1
+else
+    if [ $1 = '--local' ]; then
+        echo '##### Option set #####'
+        echo '-> For local'
+        option='-f docker-compose.local.yml'
+    else
+        exit 1
+    fi
 fi
 
 echo '##### Restart Docker container #####'
-docker-compose down
-docker-compose build
+docker-compose $option down
+docker-compose $option build
 docker image prune -f
-docker-compose up -d
+docker-compose $option up -d
